ba74b6e20df479962771e014d58c922ca2ebed09
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2023-01-17 22:07:48 -0500

    my eyes are hurting

--- flask-server/add.py
+++ flask-server/add.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask("_name__")
 
-@app.route("/add", methods = ['Post'])
-def home():
-    return render_template('add.html')
+forData = {}
+
+@app.route("/add", methods = ['Post', 'Get'])
+def add():
+    if request.method == 'Post':
+        return redirect(url_for('output'))
+    else:
+        return render_template('add.html')
+
+@app.route('/output')
+def output():
+    return render_template('output.html', nameme=formData[''])
 
     if __name__ == "__main__":
         app.run(debug=True)
