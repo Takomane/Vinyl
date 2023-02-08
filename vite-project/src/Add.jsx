204b3ce795d38cd954c1bc27e8faa552ef428dd7
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2023-02-08 18:40:49 -0500

    react addition

--- vite-project/src/Add.jsx
+++ vite-project/src/Add.jsx
@@ -1,6 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useForm } from 'react'
 import Stylesheets from './Stylesheet';
 
 function post_album()
-    
+    const {register, handleSubmit} = useForm();
+    const onSubmit =(d) =>
+    alert(JSON.stringify(d));
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>
+                Album Name:
+            <input {...register("item.Title")} />
+            </label>
+            <label>
+                Artist Name:
+            <input {...register("item.Artist")} />
+            </label>
+            <label>
+                Genre:
+            <input {...register("item.Genre")} />
+            </label>
+            <p>Render: </p>
+        </form>
+    )
 export default post_album
