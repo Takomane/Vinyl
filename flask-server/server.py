33f6b9a32a73f9f6b42799c39cbdc55e3fd4f5c8
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2023-02-08 17:52:10 -0500

    Commit

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,25 +6,15 @@ from keys import find_all_Albums
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/Albums")
+@app.route("/Albums" 'Albums/<albums_id>')
 def Albums():
-        if request.method == 'POST':
-         data = request.form.to_dict()
-         print(data)
-         ("INSERT INTO Albums (Title, Artist, Genre) VALUES (%s, %s, %s)"
-         (f"{data['Title']}", f"{data['Artist']}", f"{data['Genre']}"))
-     return 'Saved')
    return (find_all_Albums())
-
-@app.route('/Albums/<Albums_id>')
 def find_Albums_by_id(Albums_id):
     for Albums in Albums:
         if Albums["id"] == int(Albums_id):
             return jsonify({
                 "Albums":Albums,
             })
-
-@app.route('/Albums/<Albums_id>')
 def add_albums():
      if request.method == 'POST':
          data = request.form.to_dict()
