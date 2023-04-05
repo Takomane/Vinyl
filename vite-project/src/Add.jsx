19d1b6f13af4bd5201a7ea537a70c94667d1ca7b
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2023-04-05 18:27:37 -0400

    bahhhhhhhhhhhhhhhhhh

--- vite-project/src/Add.jsx
+++ vite-project/src/Add.jsx
@@ -4,10 +4,11 @@ import Stylesheets from "./Stylesheet";
 
 export default function Add() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (d) => alert(JSON.stringify(d));
+  const [data, setData] = useState("");
+  console.log(data)
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit((data) => {setData(JSON.stringify(data))})}>
       <label>
         Album Name:
         <input {...register("item.Title")} />
---
+++ vite-project/src/update.js
@@ -0,0 +1,15 @@
+useEffect(() => {
+    post("http://localhost:5000/Albums")
+      .then((response) => response.json())
+      .then((res) => setData(res));
+  }, []);
+
+  const response = await fetch(url, {
+  headers: {
+    "Content-Type": "application/json",
+    // 'Content-Type': 'application/x-www-form-urlencoded',
+  },AbortController
+
+
+  method: "POST", // *GET, POST, PUT, DELETE, etc.
+}
