61be12e58e0ed6f6ca447601e3dc5bf5372d1492
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2023-04-26 17:58:45 -0400

    ye?

--- vite-project/src/update.js
+++ vite-project/src/update.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
 
+export default function Update(){
 useEffect(() => {
     post("http://localhost:5000/Albums")
       .then((response) => response.json())
       .then((res) => setData(res));
-  }, []);
+  }, [])};
 
   const response = await fetch(url, 
   method, 'POST' , {
