97804c6a29ea6c5cf447c19888394acdad6c5308
Nick McFarlin <nick.f.mcfarlin@gmail.com>
2023-05-17 18:22:28 -0400

    useEffect hook now has dependency of newData

--- vite-project/src/update.js
+++ vite-project/src/update.js
@@ -4,15 +4,17 @@ export default function Update(){
 const [data, setData] = useState('');
 const [newData, setNewData] = useState('');
 useEffect(() => {
+  if (newData) {
   fetch("http://localhost:5000/Albums/add", {
     method: 'POST' , 
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(), 
+      body: JSON.stringify(newData), 
   })
    .then((response) => response.json())
-   .then((res) => setData(res));
-}, [data, newData])
+   .then((res) => setData(res))
+   .catch((error) => console.error(error));
+} [newData]});
 
 };
