4d9d8850d1537cb369b5e7c16db7fd2a096f80bf
takomane <nick.f.mcfarlin@gmail.com>
2023-06-14 16:34:47 -0400

    We will see ifit works from home

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,35 +6,25 @@ from keys import find_all_Albums
 app = Flask(__name__)
 CORS(app)
 
+def insert_album(title, artist, genre):
+    data = {
+        'Title': title,
+        'Artist': artist,
+        'Genre': genre
+    }
+    supabase.table('Albums').insert([data]).execute()
+
 @app.route("/Albums")
 def Albums():
    return (find_all_Albums())
-
-# @app.route('/Albums/<Albums_id>')
-# def find_Albums_by_id(Albums_id):
- #   for Albums in Albums:
-  #      if Albums["id"] == int(Albums_id):
-   #         return jsonify({
-    #            "Albums":Albums,
-     #       })
 
 @app.route('/Albums/add', methods=['GET', 'POST'])
 def add_albums():
      if request.method == 'POST':
          data = request.form.to_dict()
          print(data)
-         ("INSERT INTO Albums (Title, Artist, Genre) VALUES (%f, %f, %f)"
-         (f"{data['Title']}", f"{data['Artist']}", f"{data['Genre']}"))
+         insert_album(data['Title'], data['Artist'], data['Genre'])
      return 'Saved'
-
-#@app.route('/Albums/update/<Albums_id>', methods=['GET', 'POST'])
-#def add_albums(): (could do form data)
- #    if request.method == 'POST':
-  #       data = request.form.to_dict()
-   #      print(data)
-    #     ("INSERT INTO Albums (Title, Artist, Genre) VALUES (%f, %f, %f)"
-     #    (f"{data['Title']}", f"{data['Artist']}", f"{data['Genre']}"))
-     #return 'Saved'
 
 if __name__ == "__main__":
     app.run(debug=True)
